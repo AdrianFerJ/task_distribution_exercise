743ca0f27d1f52f569fe1bb692936dcabf548568
adrian <adrianfj@gmail.com>
2019-10-01 17:04:21 -0400

    Create objects

--- adrian_fernandez.py
+++ adrian_fernandez.py
@@ -8,34 +8,35 @@ Use case:
 
 """
 from dataclasses import dataclass
-
-@dataclass
-class Employee:
-    nickname: str
-    task_point: int
-    assigned_tasks: []
-    assigned_task_points: int
+from typing import List
 
 @dataclass
 class Task:
     name: str
     task_point: int
 
+@dataclass
+class Employee:
+    nickname: str
+    task_point: int
+    assigned_tasks = []
+    assigned_task_points: int = 0
 
-def distrbute_tasks(employes, tasks):
+
+def distrbute_tasks(employees: List[Employee], tasks: List[Task]):
     """ Finds the optimal distribution of tasks, given a group of employees"""
-    pass
+    return (employees, tasks)
 
 
 if __name__ == '__main__':
     employees_ls = []
-    employees_ls.append(Employee('e1', 1)
-    employees_ls.append(Employee('e2', 2)
-    employees_ls.append(Employee('e3', 3)
+    employees_ls.append(Employee('e1', 1))
+    employees_ls.append(Employee('e2', 2))
+    employees_ls.append(Employee('e3', 3))
 
     tasks_ls = []
     tasks_ls.append(Task('t_1', 1))
     tasks_ls.append(Task('t_2', 2))
     tasks_ls.append(Task('t_3', 3))
 
-    print(distrbute_tasks(employees_ls, tasks_ls))
+    print(distrbute_tasks(employees_ls, employees_ls))
