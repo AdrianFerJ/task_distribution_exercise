cc714a177e7690656ff2948e3f2e27796b052ba2
adrian <adrianfj@gmail.com>
2019-10-06 18:34:04 -0400

    Format output

--- adrian_fernandez.py
+++ adrian_fernandez.py
@@ -18,21 +18,31 @@ def validate_solution(solution, task_counter):
             return False
     return True      
 
+def get_tasks_names(assigned_tp):
+    """ Takes a list of tasks task_points, returns matching names."""
+    task_names = []
+    for tp in assigned_tp:
+        t_ix = tasks_tp.index(tp)
+        task_names.append(tasks_names[t_ix])
+        # Modify task value to prevent duplicate matches
+        tasks_tp[t_ix] = 0
+    return task_names
+
 def allocate_tasks(comb_tasks, target, task_counter):
     if not validate_solution(comb_tasks, task_counter):
         return False
 
     # Keep track of assignments
-    empl_assignments = [[] for i in employees]
-    empl_remaining = employees.copy()
+    empl_assignments = [[] for i in employees_tp]
+    empl_remaining = employees_tp.copy()
     total_assignments = 0
-    tasks_remaining = tasks.copy()
+    tasks_remaining = tasks_tp.copy()
     # print("Allocate tasks, comb_tasks:", comb_tasks)
 #     empl_assigned = [0 for e in employees]
 #     print(empl_assignments, empl_remaining)
     e_i = 0
     for tc_ix in range(len(comb_tasks)):
-        while e_i <= len(employees):
+        while e_i <= len(employees_tp):
 #             print("$$", comb_tasks[tc_ix], "<?>", max(empl_remaining))
             if comb_tasks[tc_ix] > max(empl_remaining):
                 break
@@ -44,11 +54,11 @@ def allocate_tasks(comb_tasks, target, task_counter):
                 tasks_remaining[tc_ix] = 0
                 total_assignments += t_tc
                 e_i += 1
-                if e_i == len(employees):
+                if e_i == len(employees_tp):
                     e_i = 0
                 break
             e_i += 1
-            if e_i == len(employees):
+            if e_i == len(employees_tp):
                 e_i = 0
     if target == total_assignments:
         print("Target!", total_assignments, empl_assignments)
@@ -62,7 +72,7 @@ def find_combinations(combs, tasks, lower_target=0):
     # Task task_point counter, for validaiton
     task_counter = Counter(tasks)
     row, col = 0, len(combs)-1
-    temp = min(sum(tasks), sum(employees))
+    temp = min(sum(tasks), sum(employees_tp))
     tp_target = temp - lower_target
     print("Target task_point:", tp_target, lower_target)
 
@@ -77,7 +87,7 @@ def find_combinations(combs, tasks, lower_target=0):
             # print("Bingo!", comb_value, "->", comb_tasks)            
             employees_tasks = allocate_tasks(comb_tasks, tp_target, task_counter)
             if employees_tasks:
-                print("!!!!! SOLUTION ", employees_tasks)
+                # print("!!!!! SOLUTION ", employees_tasks)
                 # solutions.append(employees_tasks)       
                 return employees_tasks     
             col-=1
@@ -89,11 +99,10 @@ def find_combinations(combs, tasks, lower_target=0):
         elif tp_difference > 0:
             col-=1
 
-    # If not Solution found
+    # If not Solution found, lower_target task_point total and start over
     lower_target += 1
     print("*** No Solution, lower task_point target:", lower_target)
     solution =  find_combinations(combs, tasks, lower_target)
-    print("solutions >>>", len(solution))
     return solution
 
 
@@ -101,78 +110,31 @@ def find_combinations(combs, tasks, lower_target=0):
     Run Code
 """
 if __name__ == '__main__':
-    tasks = [6,4,2,3,5]
-    tasks_name = ["T1", "T2", "T3", "T4", "T5"]
-    employees = [3,5,7]
-    emp_names = ["A", "B", "C"]
-    # tasks = [3,3,3,4,4,5]
-    # employees = [7,8,9]
+    # Define start values
+    tasks_tp = [6,3,2,3,5]
+    tasks_names = ["T1", "T2", "T3", "T4", "T5"]
+    employees_tp = [3,5,7]
+    employees_names = ["A", "B", "C"]
 
-    # tasks = [2,2,2,2,2]
-    # employees = [1,4,4,6]
-
-    # tasks = [2,2,3,3]
-    # employees = [1,4,6]
-    
-    # tasks = [1,2,2,3,3,4,4]
-    # employees = [3,5,7,9] # TODO sort decending (?)
-    # emp_names = ["A", "B", "C", "D"]
-
-    # Create dict to map tasks after sorting
-    tasks_dict = {}
-    for i in range(len(tasks)):
-        tasks_dict[tasks_name[i]] = tasks[i]
-    tasks.sort()
+    tasks_dic = {name:val for name, val in zip(tasks_names, tasks_tp)}
+    tasks_tp.sort()
+    employees_dic = {name:{'task_point':val} for name, val in zip(employees_names, 
+                                                                  employees_tp)}
+    print("Tasks", tasks_dic)                                                                
+    print("Employees", employees_dic)
 
     # Generate possible combinations of tasks, given constraints
-    combs = gen_combinations(tasks)
+    combs = gen_combinations(tasks_tp)
     combs.sort(key=lambda x: sum(x))
-    # combs_sum2 = [sum(c) for c in combs]
-    # print(combs)
-    # print(combs_sum2)
     
-    print("-------RESULT-------")
-    solutions = find_combinations(combs, tasks)
-    for s in solutions:
-        print(s)
-
-
-
-"""
-    ............................
-"""
-
-# def match_single_options(tasks, employees, emp_names):
-#     """ Match all employee's with a single task-match, remove task from tasks.
+    # call main function
+    solution = find_combinations(combs, tasks_tp)
     
-#     This is an easy way of restricting options and improving performance. 
-#     Matching attempts start from lowest to highest, requires sorted lists for
-#     employees_tp and tasks_tp. 
-
-#     There is 3 options:
-#     - No match: employee capacity is not large enough, drop this employee
-#     - Single match: allocate this task to employee, remove both from their
-#         respective lists
-#     - More than 1 match: given employee could take more than 1 task
-
-#     If all employees had either a single match or no match, this is the solution.
-
-#     Use:
-#         tasks = [1,2,3,4]
-#         emp_tp = [1,2,7]
-#         emp_names = ["A", "B", "C"]
-#         match_single_options(tasks, emp_tp, emp_names)
-#     """
-#     task_assignment = {}
-#     for e_i in range(len(employees)):
-#         emp = employees[e_i]
-#         print("#", emp, tasks)
-#         index = bisect.bisect(tasks, emp)
-#         if index == 0 or index >= len(tasks):
-#             break
-#         single_task = tasks[index-1]
-#         if single_task <= emp and tasks[index] > emp:
-#             print("Single Match", single_task, "=>", emp)
-#             task_assignment[emp_names[e_i]] = single_task
-#             tasks.remove(single_task)
-#     print(task_assignment)    
+    print("-------RESULTS-------")
+    for i in range(len(employees_names)):
+        key = employees_names[i]
+        employees_dic[key]["assigned_tasks"] = get_tasks_names(solution[i])
+        employees_dic[key]["assigned_tasks_points"] = solution[i]
+        print("Employee", key)
+        print(employees_dic[key])
+    print("--------------------")
